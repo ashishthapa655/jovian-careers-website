b4c1f31f9bc8b2e68c8d56c1d5d59cafd047173c
ashishthapa655 <133752860+ashishthapa655@users.noreply.github.com>
2023-11-21 17:44:32 +0000

    dynamic pages for each job

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db
+from database import load_jobs_from_db,load_job_from_db
 
 app = Flask(__name__)
 
@@ -10,13 +10,22 @@ app = Flask(__name__)
 @app.route("/")
 def Hello_World():
   jobs = load_jobs_from_db()
-  return render_template('home.html', jobs=jobs, company_name ='Jovian')
+  return render_template('home.html', jobs=jobs)
 
 
 @app.route("/api/jobs")
 def list_jobs():
   return jsonify(JOBS)
+
+
+@app.route("/job/<id>")
+def show_job(id):
+  job = load_job_from_db(id)
   
+  if not job:
+    return "not found", 404
+    
+  return render_template('jobpage.html', job=job )
 
 
 
